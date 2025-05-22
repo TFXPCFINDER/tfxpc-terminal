632aa5ea65658d3388f39c8d663184c3c8676e9f
Matheus <tfxpcfinder@gmail.com>
2025-05-22 15:24:45 -0300

    Teste

--- LojaVirtual/obj/Debug/net5.0/LojaVirtual.AssemblyInfo.cs
+++ LojaVirtual/obj/Debug/net5.0/LojaVirtual.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaVirtual")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15d3bfa011699952afa62556cf993d24099a4fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaVirtual")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaVirtual")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
